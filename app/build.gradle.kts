5bce579386eed788cb48502896f20d4f5aeba07f
hirose <yukihio76@gmail.com>
2026-06-22 14:40:16 +0900

    versionCode up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.yukihiro.speedWithYukihiro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
